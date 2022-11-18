669ac11a8645601b40422c7ceed95d19b55ac538
ertugrul18 <quars18@gmail.com>
2022-11-18 19:35:04 +0300

    Add project files.

---
+++ EfCoreSinema/Entities/EntityTypeConfig/Concrete/HaftaMap.cs
@@ -0,0 +1,28 @@
+﻿using EfCoreSinema.Entities.Concrete;
+using EfCoreSinema.Entities.EntityTypeConfig.Abstract;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EfCoreSinema.Entities.EntityTypeConfig.Concrete
+{
+    public class HaftaMap:BaseMap<Hafta,Guid>
+    {
+        public override void Configure(EntityTypeBuilder<Hafta> builder)
+        {
+            base.Configure(builder);
+            builder.Property(p => p.HaftaAdi).HasMaxLength(50);
+            builder.Property(p => p.Baslangic)
+                .HasColumnType("datetime")
+                .IsRequired(false);
+            builder.Property(p => p.Bitis)
+                .HasColumnType("datetime")
+                .IsRequired(false);
+
+        }
+    }
+}
