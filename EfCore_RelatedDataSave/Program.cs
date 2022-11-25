e4d7e55516424dfdc1cdbbbd5ceb84f69b0fa8e8
ertugrul18 <quars18@gmail.com>
2022-11-25 19:22:38 +0300

    .

--- EfCore_RelatedDataSave/Program.cs
+++ EfCore_RelatedDataSave/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace EfCore_RelatedDataSave
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EfCore_RelatedDataSave
 {
     internal class Program
     {
@@ -140,6 +142,92 @@
             //context.SaveChanges();
 
             #endregion
+
+            #region Many-To-Many Kayıt Güncelleme
+            //// 3 numaralı kitabı 1 numaralı yazara atamak istediğinizde
+
+            ////Kitaba konumlan
+            //Kitap? kitap = context.Kitaplar.Find(3);
+
+            ////Yeni yazara konumlan
+            //Yazar? yeniyazar = context.Yazarlar.Find(1);
+
+            ////Eski yazara konumlan
+            //Yazar? eskiyazar = context.Yazarlar.
+            //    Include(p => p.Kitaplar).
+            //    FirstOrDefault(p => p.Id == 3);
+
+            ////Eski yazarı kaldır
+            //eskiyazar.Kitaplar.Remove(kitap);
+
+            ////kitabu yeni yazara ekle
+            //kitap.Yazarlar.Add(yeniyazar);
+
+            //var objects = context.ChangeTracker;
+
+            //context.SaveChanges();
+
+
+            #endregion
+
+            #region One-to-Many Kayıt Güncelleme (blok principle - dependent(çoğul) postlar)
+            // Blog 3 için yapılan en son yorumu blog 2 ye aktarın
+            /*
+            1- Eski Blog'a konumlan
+            2- Post'a konumlan
+            3- Eski Blog'dan Remove et.
+            4- Yeni blog'a konumlan
+            5- Yeni Blog'a ekle
+             */
+
+            // 1- Eski Blog'a konumlan
+            //Blog? eskiblog = context.Bloglar.Include(p => p.Posts).FirstOrDefault(p => p.Id == 3);
+
+            //// 2- Post'a konumlan
+            //Post? post = context.Postlar.FirstOrDefault(p => p.Id == 8);
+
+            //// 3- Eski Blog'dan Remove et.
+            //eskiblog.Posts.Remove(post);
+
+            ////  4-5 Yeni blog'a konumlan
+            //Blog? yeniBlog = context.Bloglar.Find(2);
+            //yeniBlog.Posts.Add(post);
+
+
+            //context.SaveChanges();
+
+            #endregion
+
+            #region Bağımlı verilerin ilişkisel olduğu ana veriyi güncelelme (Dependent Entity'nin Principal Entity'sini güncelleme)
+            //6 numaralı post'u 3 numaralı Blog'a atama islemi
+
+            //Post? post = context.Postlar.Find(6);
+            //Blog? blog = context.Bloglar.Find(3);
+
+            //post.Blog = blog;
+
+            //context.SaveChanges();
+
+
+            #endregion
+
+            #region One-To-Many Kayit Silme
+
+            //Blog? blog = context.Bloglar.Include(p => p.Posts).FirstOrDefault(p => p.Id == 3);
+            //blog.Posts.Clear();
+            //context.SaveChanges();
+
+
+            Blog? blog = context.Bloglar.Include(p => p.Posts).FirstOrDefault(p => p.Id == 2);
+            
+            context.Bloglar.Remove(blog);
+
+            context.SaveChanges();
+
+            #endregion
+
+
+
         }
     }
 }
